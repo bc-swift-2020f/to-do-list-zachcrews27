c5b63d7e6f38045efcee931ec8500e23dfbf3064
Zach Crews <crewsz@bc.edu>
2020-10-04 19:41:16 -0400

    Completed 5.15

---
+++ ToDo List/ListTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  ListTableViewCell.swift
+//  ToDo List
+//
+//  Created by Zach Crews on 10/3/20.
+//  Copyright © 2020 Zachary Crews. All rights reserved.
+//
+
+import UIKit
+
+protocol ListTableViewCellDelegate: class {
+    
+    func checkBoxToggle(sender: ListTableViewCell)
+    
+}
+
+class ListTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var checkBoxButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    weak var delegate: ListTableViewCellDelegate?
+    
+    var toDoItem: ToDoItem! {
+        didSet {
+            nameLabel.text = toDoItem.name
+            checkBoxButton.isSelected = toDoItem.completed
+        }
+    }
+
+    @IBAction func checkToggled(_ sender: UIButton) {
+        delegate?.checkBoxToggle(sender: self)
+    }
+}
---
+++ ToDo List/ToDoItems.swift
@@ -0,0 +1,58 @@
+//
+//  ToDoItems.swift
+//  ToDo List
+//
+//  Created by Zach Crews on 10/4/20.
+//  Copyright © 2020 Zachary Crews. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+class ToDoItems {
+    var itemsArray: [ToDoItem] = []
+    
+    func saveData() {
+        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let documentURL = directoryURL.appendingPathComponent("todos").appendingPathExtension("json")
+        
+        let jsonEncoder = JSONEncoder()
+        let data = try? jsonEncoder.encode(itemsArray)
+        do {
+            try data?.write(to: documentURL, options: .noFileProtection)
+        } catch {
+            print("ERROR: Could not save data \(error.localizedDescription)")
+        }
+        setNotifications()
+    }
+        
+    func loadData(completed: @escaping () -> ()) {
+        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let documentURL = directoryURL.appendingPathComponent("todos").appendingPathExtension("json")
+
+        guard let data = try? Data(contentsOf: documentURL) else {return}
+        let jsonDecoder = JSONDecoder()
+        do {
+            itemsArray = try jsonDecoder.decode(Array<ToDoItem>.self, from: data)
+        } catch {
+            print("ERROR: Could not load data \(error.localizedDescription)")
+        }
+    }
+    
+    func setNotifications() {
+        guard itemsArray.count > 0 else {
+            return
+        }
+        
+        // remove all notifications
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        
+        // and let's recreate them with the data that we just saved
+        for index in 0..<itemsArray.count {
+            if itemsArray[index].reminderSet {
+                let toDoItem = itemsArray[index]
+                itemsArray[index].notificationID = LocalNotificationManager.setCalendarNotification(title: toDoItem.name, subtitle: "", body: toDoItem.notes, badgeNumber: nil, sound: .default, date: toDoItem.date)
+            }
+        }
+    }
+}
